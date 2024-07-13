04d5852381d472c32d1e73afcf08646dbbed3adf
KaanAkbay <thedosen48@gmail.com>
2024-07-13 15:06:54 +0300

    Solved double LList

--- hw1/ps1-template/Doubly_Linked_List_Seq.py
+++ hw1/ps1-template/Doubly_Linked_List_Seq.py
@@ -61,26 +61,51 @@ class Doubly_Linked_List_Seq:
 
     def delete_first(self):
         x = None
-        
-
+        x= self.head.item
+        if(self.head.next == None):
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
         return x
 
     def delete_last(self):
         x = None
-        ###########################
-        # Part (a): Implement me! #
-        ###########################
+        x = self.tail.item
+        if(self.tail.prev == None):
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None    
         return x
 
     def remove(self, x1, x2):
         L2 = Doubly_Linked_List_Seq()
-        ###########################
-        # Part (b): Implement me! # 
-        ###########################
+        L2.head = x1
+        L2.tail = x2
+        if(x1 == self.head):
+            self.head = x2.next
+        else:
+            x1.prev.next = x2.next
+        if(x2==self.tail):
+            self.tail = x1.prev
+        else:
+            x2.next.prev = x1.prev               
         return L2
 
     def splice(self, x, L2):
-        ###########################
-        # Part (c): Implement me! # 
-        ###########################
+        x1 = L2.head
+        x2 = L2.tail
+        x3 = x.next
+        L2.head = None
+        L2.tail = None
+        x1.prev = x
+        x.next= x1
+        x2.next = x3
+        if(x3 == None):
+            x2 =self.tail
+        else:
+            x3.prev = x2    
         pass
