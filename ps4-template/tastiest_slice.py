adb52f7c69fe62268799ae91289794d49ef59828
KaanAkbay <thedosen48@gmail.com>
2024-08-22 17:20:06 +0300

    ps4 soln

--- ps4-template/tastiest_slice.py
+++ ps4-template/tastiest_slice.py
@@ -15,35 +15,47 @@ class Key_Val_Item:
 class Part_B_Node(BST_Node):
     def subtree_update(A):
         super().subtree_update()
-        #########################################
-        # ADD ANY NEW SUBTREE AUGMENTATION HERE #
-        #########################################
+        A.sum = A.item.val # sum
+        if A.left: 
+            A.sum += A.left.sum
+        if A.right: 
+            A.sum += A.right.sum
+        left = float('-inf') 
+        right = float('-inf')
+        mid = A.item.val
+        if A.left:
+            left = A.left.max_prefix
+            mid +=A.left.sum
+        if A.right:
+            right = mid + A.right.max_prefix
+        A.max_prefix = max(left,mid,right)
+        if left == A.max_prefix: # max prefix key
+              A.max_prefix_key = A.left.max_prefix_key
+        elif mid == A.max_prefix:
+            A.max_prefix_key = A.item.key
+        else:
+            A.max_prefix_key = A.right.max_prefix_key        
 
 class Part_B_Tree(Set_AVL_Tree):
     def __init__(self): 
         super().__init__(Part_B_Node)
 
     def max_prefix(self):
-        '''
-        Output: (k, s) | a key k stored in tree whose
-                       | prefix sum s is maximum
-        '''
+        
         k, s = 0, 0
-        ##################
-        # YOUR CODE HERE #
-        ##################
+        k=self.root.max_prefix_key
+        s = self.root.max_prefix
         return (k, s)
 
 def tastiest_slice(toppings):
-    '''
-    Input:  toppings | List of integer tuples (x,y,t) representing 
-                     | a topping at (x,y) with tastiness t
-    Output: tastiest | Tuple (X,Y,T) representing a tastiest slice 
-                     | at (X,Y) with tastiness T
-    '''
-    B = Part_B_Tree()   # use data structure from part (b)
+   
+    B = Part_B_Tree()   
     X, Y, T = 0, 0, 0
-    ##################
-    # YOUR CODE HERE #
-    ##################
+    length=len(toppings)
+    toppings.sort(key=lambda topping :topping[0])
+    for(x,y,t) in toppings:
+        B.insert(Key_Val_Item(y,t))
+        (mY,mT) = B.max_prefix()
+        if T<mT:
+            X,Y,T=x,mY,mT
     return (X, Y, T)
