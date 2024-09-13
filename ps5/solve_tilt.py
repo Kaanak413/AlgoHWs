f4d7400cdfca9b9e4aca5aaecb1082f5c7fd40c2
KaanAkbay <thedosen48@gmail.com>
2024-09-13 13:40:51 +0300

    5th ps solution

---
+++ ps5/solve_tilt.py
@@ -0,0 +1,91 @@
+from collections import deque
+
+def solve_tilt(B, t):
+   
+    xt, yt = t  # Target coordinates
+
+    # BFS setup
+    queue = deque([(B, [])])  # Store (board configuration, move sequence)
+    visited = set()  # Keep track of visited configurations
+    visited.add(B)  # Mark the initial configuration as visited
+
+    # BFS loop
+    while queue:
+        current_board, current_moves = queue.popleft()
+
+        # Check if any slider is at the target
+        if current_board[yt][xt] == 'o':
+            return current_moves  # Return the sequence of moves to solve
+
+        # Explore moves in all 4 directions: up, down, left, right
+        for direction in ['up', 'down', 'left', 'right']:
+            new_board = move(current_board, direction)
+
+            # If the new configuration is valid and hasn't been visited
+            if new_board and new_board not in visited:
+                visited.add(new_board)  # Mark the new configuration as visited
+                queue.append((new_board, current_moves + [direction]))  # Append new state
+
+    return None
+####################################
+# USE BUT DO NOT MODIFY CODE BELOW #
+####################################
+def move(B, d):
+    '''
+    Input:  B  | Board configuration
+            d  | Direction: either 'up', down', 'left', or 'right'
+    Output: B_ | New configuration made by tilting B in direction d
+    '''
+    n = len(B)
+    B_ = list(list(row) for row in B)
+    if d == 'up':
+        for x in range(n):  
+            y_ = 0          
+            for y in range(n):
+                if (B_[y][x] == 'o') and (B_[y_][x] == '.'):
+                    B_[y][x], B_[y_][x] = B_[y_][x], B_[y][x]
+                    y_ += 1
+                if (B_[y][x] != '.') or (B_[y_][x] != '.'):
+                    y_ = y
+    if d == 'down':
+        for x in range(n):  
+            y_ = n - 1
+            for y in range(n - 1, -1, -1):
+                if (B_[y][x] == 'o') and (B_[y_][x] == '.'):
+                    B_[y][x], B_[y_][x] = B_[y_][x], B_[y][x]
+                    y_ -= 1
+                if (B_[y][x] != '.') or (B_[y_][x] != '.'):
+                    y_ = y
+    if d == 'left':
+        for y in range(n):  
+            x_ = 0          
+            for x in range(n):
+                if (B_[y][x] == 'o') and (B_[y][x_] == '.'):
+                    B_[y][x], B_[y][x_] = B_[y][x_], B_[y][x]
+                    x_ += 1
+                if (B_[y][x] != '.') or (B_[y][x_] != '.'):
+                    x_ = x
+    if d == 'right':
+        for y in range(n):  
+            x_ = n - 1
+            for x in range(n - 1, -1, -1):
+                if (B_[y][x] == 'o') and (B_[y][x_] == '.'):
+                    B_[y][x], B_[y][x_] = B_[y][x_], B_[y][x]
+                    x_ -= 1
+                if (B_[y][x] != '.') or (B_[y][x_] != '.'):
+                    x_ = x
+    B_ = tuple(tuple(row) for row in B_)
+    return B_
+
+def board_str(B):
+    '''
+    Input:  B | Board configuration
+    Output: s | ASCII string representing configuration B
+    '''
+    n = len(B)
+    rows = ['+' + ('-'*n) + '+']
+    for row in B:
+        rows.append('|' + ''.join(row) + '|')
+    rows.append(rows[0])
+    S = '\n'.join(rows)
+    return S
